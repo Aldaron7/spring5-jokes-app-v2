5ce4c7a5bf85e4b2ecd2be8437e32c021f983ec3
Marcel Vollmer <mvr@aformatik.de>
2022-07-22 11:55:29 +0200

    added JokeService

---
+++ src/main/java/de/mvr/jokesapp/services/JokeService.java
@@ -0,0 +1,6 @@
+package de.mvr.jokesapp.services;
+
+public interface JokeService
+{
+  String getJoke();
+}
---
+++ src/main/java/de/mvr/jokesapp/services/JokeServiceImpl.java
@@ -0,0 +1,18 @@
+package de.mvr.jokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService
+{
+  private final ChuckNorrisQuotes chuckNorrisQuotes = new ChuckNorrisQuotes();
+
+  public JokeServiceImpl() {}
+
+  @Override
+  public String getJoke()
+  {
+    return chuckNorrisQuotes.getRandomQuote();
+  }
+}
