b6d9e1af02d0c154b2b633177e1ac6cae25fbe91
Marcel Vollmer <mvr@aformatik.de>
2022-07-22 11:19:12 +0200

    initial commit

---
+++ src/main/java/de/mvr/jokesapp/JokesAppApplication.java
@@ -0,0 +1,15 @@
+package de.mvr.jokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesAppApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(JokesAppApplication.class, args);
+  }
+
+}
